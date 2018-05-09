0cedffb8c026dc8263afb5905f5774535d38134b
Yorick de Boer <mail@yorickdeboer.nl>
2018-05-09 13:13:27 +0200

    more new features and setup multiprocess

--- assignment1/generate_aggregated_features.py
+++ assignment1/generate_aggregated_features.py
@@ -1,13 +1,14 @@
 import sqlite3
-from pprint import pprint
 from dateutil.relativedelta import relativedelta
 
-from assignment1 import load_data, postprocess_data
-from datetime import datetime
+from assignment1 import load_data
 import progressbar
+from multiprocessing import Pool, Process
 
 
-def create(conn, data: list):
+def create(data: list):
+    conn = sqlite3.connect(':memory:')
+
     c = conn.cursor()
     c.execute('''CREATE TABLE transactions
                  (id int, booking_date date, issuer_country text, tx_variant text, issuer_id float, amount float, 
@@ -22,8 +23,12 @@ def create(conn, data: list):
     c.executemany('''INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', rows)
     conn.commit()
 
+    return conn
 
-def txn_amount_over_month(conn, data: list):
+
+def txn_amount_over_month(data: list):
+    conn = create(data)
+
     print('txn_amount_over_month')
     bar = progressbar.ProgressBar(max_value=len(data))
 
@@ -38,9 +43,12 @@ def txn_amount_over_month(conn, data: list):
             f.write('{}\n'.format(sum([amount[0] for amount in transactions]) / 30))
 
             bar.update(idx)
+    conn.close()
 
 
-def average_daily_over_month(conn, data: list):
+def average_daily_over_month(data: list):
+    conn = create(data)
+
     print('average_daily_over_month')
     bar = progressbar.ProgressBar(max_value=len(data))
 
@@ -55,17 +63,208 @@ def average_daily_over_month(conn, data: list):
             f.write('{}\n'.format(sum([amount[0] for amount in transactions]) / 30))
 
             bar.update(idx)
+    conn.close()
+
+
+def amount_same_day(data: list):
+    conn = create(data)
+
+    print('amount_same_day')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('amount_same_day.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(days=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT amount FROM transactions WHERE card_id = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['card_id'], begin, row['creation_date']))
+            transactions = c.fetchall()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions])))
+
+            bar.update(idx)
+    conn.close()
+
+
+def number_same_day(data: list):
+    conn = create(data)
+
+    print('number_same_day')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('number_same_day.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(days=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT COUNT(*) FROM transactions WHERE card_id = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['card_id'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([count[0] for count in transactions])))
+
+            bar.update(idx)
+    conn.close()
+
+
+def amount_same_merchant_month(data: list):
+    conn = create(data)
+
+    print('amount_same_merchant_month')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('number_same_day.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(days=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT amount FROM transactions WHERE account_code = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['account_code'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions]) / 30))
+
+            bar.update(idx)
+
+
+def number_same_merchant_month(data: list):
+    conn = create(data)
+
+    print('number_same_merchant_month')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('number_same_merchant_month.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(months=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT COUNT(*) FROM transactions WHERE account_code = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['account_code'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions])))
+
+            bar.update(idx)
+    conn.close()
+
+
+def amount_same_currency_month(data: list):
+    conn = create(data)
+
+    print('amount_same_currency_month')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('amount_same_currency_month.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(months=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT amount FROM transactions WHERE currency = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['currency'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions]) / 30))
+
+            bar.update(idx)
+    conn.close()
+
+
+def number_same_currency_month(data: list):
+    conn = create(data)
+
+    print('number_same_currency_month')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('number_same_currency_month.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(months=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT COUNT(*) FROM transactions WHERE currency = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['currency'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions])))
+
+            bar.update(idx)
+    conn.close()
+
+
+def amount_same_shopper_country_month(data: list):
+    conn = create(data)
+
+    print('amount_same_shopper_country_month')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('amount_same_shopper_country_month.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(months=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT amount FROM transactions WHERE shopper_country = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['shopper_country'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions]) / 30))
+
+            bar.update(idx)
+
+
+def number_same_shopper_country_month(data: list):
+    conn = create(data)
+
+    print('number_same_shopper_country_month')
+    bar = progressbar.ProgressBar(max_value=len(data))
+
+    with open('number_same_shopper_country_month.csv', mode='w') as f:
+        for idx, row in enumerate(data):
+            begin = row['creation_date'] + relativedelta(months=-1)
+            c = conn.cursor()
+            c.execute(
+                '''SELECT COUNT(*) FROM transactions WHERE shopper_country = ? AND creation_date BETWEEN date(?) AND date(?)''',
+                (row['shopper_country'], begin, row['creation_date']))
+            transactions = c.fetchone()
+            f.write('{}\n'.format(sum([amount[0] for amount in transactions])))
+
+            bar.update(idx)
+    conn.close()
+
+
+def create_all(data: list):
+    p1 = Process(target=txn_amount_over_month, args=(data,))
+    p1.start()
+    p2 = Process(target=txn_amount_over_month, args=(data,))
+    p2.start()
+    p3 = Process(target=average_daily_over_month, args=(data,))
+    p3.start()
+    p4 = Process(target=amount_same_day, args=(data,))
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+
+    p1 = Process(target=number_same_day, args=(data,))
+    p1.start()
+    p2 = Process(target=amount_same_merchant_month, args=(data,))
+    p2.start()
+    p3 = Process(target=number_same_merchant_month, args=(data,))
+    p3.start()
+    p4 = Process(target=amount_same_currency_month, args=(data,))
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+
+    p1 = Process(target=number_same_currency_month, args=(data,))
+    p1.start()
+    p2 = Process(target=amount_same_shopper_country_month, args=(data,))
+    p2.start()
+    p3 = Process(target=number_same_shopper_country_month, args=(data,))
+    p3.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
 
 
 if __name__ == '__main__':
-    conn = sqlite3.connect('data.db')
-
-    data, categorical_sets = load_data()
-    postprocessed_data = postprocess_data(data)
-
-    # create(conn, postprocessed_data)
-
-    txn_amount_over_month(conn, postprocessed_data)
-    average_daily_over_month(conn, postprocessed_data)
-
-    conn.close()
+    data, categorical_sets = load_data(use_cached=False)
+    create_all(data)
