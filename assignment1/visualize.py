0b818bb45d6f4508f669a3411447e17ba04b4c09
Sophi <sophieclaus5@hotmai.com>
2018-05-09 15:00:41 +0200

    Can't transform all columns at once.

--- assignment1/visualize.py
+++ assignment1/visualize.py
@@ -218,7 +218,8 @@ def plot_roc_curve_kfold(fitted_classifiers: list, set_x_test: list, set_y_test:
 
 def plot_decision_tree(clf, feature_names):
     export_graphviz(clf.estimators_[0],
-                    class_names=feature_names,
+                    feature_names=feature_names,
+                    class_names=['fraud','nonfraud'],
                     out_file='dotfile.dot',
                     filled=True,
                     rounded=True)
