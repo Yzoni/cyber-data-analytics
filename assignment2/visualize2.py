112c7c23c8678b7b61af08f3b8e33781d5d50755
Sophi <sophieclaus5@hotmai.com>
2018-05-21 18:12:21 +0200

    Assignment2

---
+++ assignment2/visualize2.py
@@ -0,0 +1,9 @@
+from os import system
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy import interp
+from pandas import Series
+from matplotlib import pyplot
+
